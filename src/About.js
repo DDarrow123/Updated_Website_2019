9cdf7533c2584746c8e52ea7c3ace8e1c9664b06
DDarrow123 <ddarrowphl@gmail.com>
2019-04-10 16:13:53 -0400

    built out skeleton of about and work sections

--- src/About.js
+++ src/About.js
@@ -4,6 +4,7 @@ class About extends Component {
   render() {
     return (
       <div className="about-para">
+        <h5 className="about-para__about-title">About</h5>
         <p className="about-para__about-description">
           Here is a description about me!
         </p>
