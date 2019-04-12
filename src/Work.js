bb809af8d2ee2fe4637e13b0405d016d2c6085f9
DDarrow123 <ddarrowphl@gmail.com>
2019-04-12 18:24:10 -0400

    added scroll function to work section

--- src/Work.js
+++ src/Work.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import gallery_site from "./images/gallery_site.png";
 
 class Work extends Component {
+  activateScroll = () => {
+    window.scroll({
+      top: 2700,
+      behavior: "smooth"
+    });
+  };
   render() {
-    return <div className=""> Work Section </div>;
+    return (
+      <div onMouseEnter={this.activateScroll} className="work-container">
+        <div className="work-container__work-title">
+          SELECTED <br /> WORK
+        </div>
+        <div className="work-container__project-container">
+          <div className="work-1">
+            <img src={gallery_site} />
+          </div>
+          <div className="work-2">Gallery Site</div>
+          <div className="work-3">Gallery Site</div>
+        </div>
+      </div>
+    );
   }
 }
 
