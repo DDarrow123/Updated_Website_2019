ff508ff91a7a5a53f3547e266c6f035e906bfd85
DDarrow123 <ddarrowphl@gmail.com>
2019-04-29 15:29:32 -0400

    working on work section functionality

--- src/Work.js
+++ src/Work.js
@@ -2,11 +2,42 @@ import React, { Component } from "react";
 import gallery_site from "./images/gallery_site.png";
 
 class Work extends Component {
+  state = {
+    hover: null
+  };
   activateScroll = () => {
     window.scroll({
-      top: 2700,
+      top: 2600,
       behavior: "smooth"
     });
+  };
+
+  renderProjectTitle = e => {
+    if (e.target.className === "work-1") {
+      this.setState(
+        {
+          hover: true
+        },
+        () => console.log("hello!")
+      );
+    } else if (e.target.className === "work-2") {
+    }
+
+    // this.setState(
+    //   {
+    //     hover: true
+    //   },
+    //   () => console.log("hello!")
+    // );
+  };
+
+  hideProjectTitle = () => {
+    this.setState(
+      {
+        hover: false
+      },
+      () => console.log("goodbye!")
+    );
   };
   render() {
     return (
@@ -15,10 +46,24 @@ class Work extends Component {
           SELECTED <br /> WORK
         </div>
         <div className="work-container__project-container">
-          <div className="work-1">
-            <img src={gallery_site} />
+          <div
+            className="work-1"
+            onMouseLeave={this.hideProjectTitle}
+            onMouseEnter={this.renderProjectTitle}
+          >
+            {this.state.hover ? (
+              <h5 className="hover-title">Gallery Project</h5>
+            ) : (
+              <img src={gallery_site} />
+            )}
           </div>
-          <div className="work-2">Gallery Site</div>
+          <div
+            className="work-2"
+            onMouseEnter={() => this.renderProjectTitle()}
+            onMouseLeave={() => this.hideProjectTitle()}
+          >
+            {this.state.hover ? <h5 className="hover-title">Test 2</h5> : null}
+          </div>
           <div className="work-3">Gallery Site</div>
         </div>
       </div>
